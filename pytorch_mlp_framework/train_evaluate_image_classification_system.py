1b702282b943ba507750a7235b6291526fbe0cf8
cramptc <cramptc@hotmail.com>
2024-11-20 19:53:27 +0000

    First Commit to Fork
    Attempt

--- pytorch_mlp_framework/train_evaluate_image_classification_system.py
+++ pytorch_mlp_framework/train_evaluate_image_classification_system.py
@@ -44,6 +44,15 @@ test_data_loader = DataLoader(test_data, batch_size=args.batch_size, shuffle=Tru
 if args.block_type == 'conv_block':
     processing_block_type = ConvolutionalProcessingBlock
     dim_reduction_block_type = ConvolutionalDimensionalityReductionBlock
+elif args.block_type == 'res_block':
+    processing_block_type = ConvolutionalProcessingBlockRC
+    dim_reduction_block_type = ConvolutionalDimensionalityReductionBlockBNRC
+elif args.block_type == 'res_block_bn':
+    processing_block_type = ConvolutionalProcessingBlockBNRC
+    dim_reduction_block_type = ConvolutionalDimensionalityReductionBlockBNRC
+elif args.block_type == 'bn':
+    processing_block_type = ConvolutionalProcessingBlockBN
+    dim_reduction_block_type = ConvolutionalDimensionalityReductionBlockBNRC
 elif args.block_type == 'empty_block':
     processing_block_type = EmptyBlock
     dim_reduction_block_type = EmptyBlock
